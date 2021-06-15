e714cff385a997d7aeff05ce575c848ea63ca248
emondd4 <emondd4@gmail.com>
2021-06-15 12:09:18 +0600

    "12.09 PM 15/06/2021"

--- quiz_application/lib/main.dart
+++ quiz_application/lib/main.dart
@@ -8,11 +8,22 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
+  var questions = ["What's Your Favourite Color?","What's Your Favourite Game?", "What's Your Favourite Porn Site?"];
+
+  void onButtonPressed(){
+    print("Clicked");
+  }
+
+  void onLongButtonPressed(){
+    print("Long Press Clicked");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,10 +38,13 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(children: [
           Align(alignment: Alignment(0.0,-50.0),),
-          Text("The Question!"),
-          RaisedButton(child: Text("Answer 1"), onPressed: null,),
-          RaisedButton(child: Text("Answer 2"), onPressed: null,),
-          RaisedButton(child: Text("Answer 3"), onPressed: null,),
+          Text(questions.elementAt(0)),
+          ElevatedButton(child: Text("Answer 1"), onPressed: onButtonPressed, onLongPress: onLongButtonPressed,),
+          ElevatedButton(child: Text("Answer 2"), onPressed: () => print("Second Button Pressed")),
+          ElevatedButton(child: Text("Answer 3"), onPressed: () {
+            //////
+            print("Third Button Pressed");
+          }, onLongPress: onLongButtonPressed,),
         ],),
       ),
     );
