40276a2a48c708bf822cbf26b865aebcc8ff292a
Emon Hossain <emondd4@gmail.com>
2021-09-28 17:10:29 +0600

    set state and color done

--- willir/lib/Activities/DashboardScreen.dart
+++ willir/lib/Activities/DashboardScreen.dart
@@ -11,12 +11,26 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+
+  var selectedIndex = 0;
+  var _isSelected = false;
+
   @override
   Widget build(BuildContext context) {
     var totalHeight = MediaQuery
         .of(context)
         .size
         .height;
+
+    var totalWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+
+    final _containerList = [
+      homeContainer(totalWidth),
+      profileContainer(totalWidth),
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -64,137 +78,160 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
         ),
       ),
-      body: Container(
-        height: totalHeight,
-        child: homeContainer(context),
-      ),
-      bottomNavigationBar: navContainer(totalHeight),
-    );
-  }
-
-  Container navContainer(double totalHeight) {
-    return Container(
-      height: totalHeight * .09,
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-            boxShadow: [
-              BoxShadow(blurRadius: 20, color: Colors.grey, spreadRadius: 1)
-            ]),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            buildContainerBottomNav(Icons.home, isSelected: true),
-            buildContainerBottomNav(Icons.person),
-            buildContainerBottomNavSpecial(Icons.calendar_today_outlined),
-            buildContainerBottomNav(Icons.credit_card),
-            buildContainerBottomNav(Icons.menu),
-          ],
-        ),
-      ),
-    );
-  }
-
-  homeContainer(BuildContext context) {
-    return Container(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Most Requested",
-                  style: CustomTextStyle.AttentionCyan,
-                  textAlign: TextAlign.start,
-                ),
-
-                SizedBox(
-                  height: 10,
-                ),
-
-                Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  clipBehavior: Clip.antiAlias,
-                  child: Container(
-                    width: MediaQuery
-                        .of(context)
-                        .size
-                        .width,
-                    height: 180,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage(
-                              "assets/images/dummy_doc.jpg",
-                            ),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5)),
-                          color: CustomColors.White,
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Text(
-                              "Book Now",
-                              style: CustomTextStyle.ButtonTextCyanSmall,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+      body: _containerList[selectedIndex],
+      // body: Container(
+      //   height: totalHeight,
+      //   child:
+      // ),
+      bottomNavigationBar: Container(
+        height: totalHeight * .09,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              boxShadow: [
+                BoxShadow(blurRadius: 20, color: Colors.grey, spreadRadius: 1)
+              ]),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  child: Icon(
+                    Icons.home,
+                    color: selectedIndex ==0 ? CustomColors.PrimaryColor: CustomColors.Grey,
+                    size: 32.0,
                   ),
                 ),
-
-                SizedBox(
-                  height: 10,
+                onTap: () {
+                  setState(() {
+                    selectedIndex = 0;
+                  });
+                },
+              ),
+              InkWell(
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  child: Icon(Icons.person,
+                    color: selectedIndex ==1 ? CustomColors.PrimaryColor: CustomColors.Grey, size: 32.0,),
                 ),
-
-                GridView(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,crossAxisSpacing: 10,mainAxisSpacing: 10),
-                  children: [
-                    buildCard(
-                        "assets/images/dashboard_checkin.png", "Check In"),
-                    buildCard(
-                        "assets/images/dashboard_friends.png", "Friends Offer"),
-                    buildCard(
-                        "assets/images/dashboard_pharmacy.png", "Pharmacy"),
-                    buildCard(
-                        "assets/images/dashboard_service.png", "Our Service"),
-                    buildCard(
-                        "assets/images/dashboard_doctors.png", "Our Doctors"),
-                    buildCard(
-                        "assets/images/dashboard_video.png", "Live Video Consult"),
-                    buildCard(
-                        "assets/images/dashboard_points.png", "My Points"),
-                    buildCard(
-                        "assets/images/dashboard_directions.png", "Directions"),
-                    buildCard(
-                        "assets/images/dashboard_contact.png", "Contact Us"),
-                  ],
-                ),
-              ],
-            ),
+                onTap: () {
+                  setState(() {
+                    selectedIndex = 1;
+                  });
+                },
+              ),
+              buildContainerBottomNavSpecial(Icons.calendar_today_outlined),
+              buildContainerBottomNav(Icons.credit_card, 2),
+              buildContainerBottomNav(Icons.menu, 3),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 
-  buildContainerBottomNav(IconData icon, {isSelected = false}) {
+  homeContainer(double totalWidth) {
     return Container(
-      height: 60,
-      width: 60,
-      child: Icon(icon,
-          color: isSelected ? CustomColors.PrimaryColor : CustomColors.Grey, size: 32.0,),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Most Requested",
+                style: CustomTextStyle.AttentionCyan,
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                clipBehavior: Clip.antiAlias,
+                child: Container(
+                  width: totalWidth,
+                  height: 180,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                            "assets/images/dummy_doc.jpg",
+                          ),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        color: CustomColors.White,
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Book Now",
+                            style: CustomTextStyle.ButtonTextCyanSmall,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              GridView(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
+                children: [
+                  buildCard("assets/images/dashboard_checkin.png", "Check In"),
+                  buildCard(
+                      "assets/images/dashboard_friends.png", "Friends Offer"),
+                  buildCard("assets/images/dashboard_pharmacy.png", "Pharmacy"),
+                  buildCard(
+                      "assets/images/dashboard_service.png", "Our Service"),
+                  buildCard(
+                      "assets/images/dashboard_doctors.png", "Our Doctors"),
+                  buildCard("assets/images/dashboard_video.png",
+                      "Live Video Consult"),
+                  buildCard("assets/images/dashboard_points.png", "My Points"),
+                  buildCard(
+                      "assets/images/dashboard_directions.png", "Directions"),
+                  buildCard(
+                      "assets/images/dashboard_contact.png", "Contact Us"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildContainerBottomNav(IconData icon, int itemNumber) {
+    return InkWell(
+      child: Container(
+        height: 60,
+        width: 60,
+        child: Icon(
+          icon,
+          color: CustomColors.Grey,
+          size: 32.0,
+        ),
+      ),
+      onTap: () {},
     );
   }
 
@@ -235,17 +272,22 @@ class _DashboardScreenState extends State<DashboardScreen> {
           children: [
             Image.asset(
               image,
-              height: 60,
-              width: 60,
+              height: 40,
+              width: 40,
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 5,),
             Text(
               title,
               style: CustomTextStyle.ButtonTextCyanSmall,
+              textAlign: TextAlign.center,
             )
           ],
         ));
+  }
+
+  profileContainer(double totalWidth) {
+    return Container(
+      child: Text("Profile Fragment"),
+    );
   }
 }
