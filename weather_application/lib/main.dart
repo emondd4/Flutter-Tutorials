ae7135e809008b02dd17347bee5ae5d8934e979b
Emon Hossain <emondd4@gmail.com>
2021-09-30 21:32:14 +0600

    weather app init

---
+++ weather_application/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFf9f9f9),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFf9f9f9),
+        elevation: 0.0,
+        title: Text(
+          'Weather Application',
+          style: GoogleFonts.poppins(fontStyle: FontStyle.normal),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+          color: Colors.black,
+        ),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
