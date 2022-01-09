6166e10e82102af22a7876ae2c357b192082aa11
Emon Hossain <emondd4@gmail.com>
2022-01-09 23:39:29 +0600

    basic dialogue

---
+++ Dialogs/flutter_dialogs/lib/HomeView.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Dialogs"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('AlertDialog Title'),
+                    content: const Text('AlertDialog description'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'OK'),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: const Text("Alert Dialog"),
+              style: ElevatedButton.styleFrom(primary: Colors.purple),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
