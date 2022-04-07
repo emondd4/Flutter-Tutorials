b7a5353b8ffdfe24684f44ee137650257ef8ccf4
Emon Hossain <emondd4@gmail.com>
2022-04-07 11:08:33 +0600

    final bump animated splash screen with native splash

--- flutter_animated_splash/lib/Views/SplashScreen.dart
+++ flutter_animated_splash/lib/Views/SplashScreen.dart
@@ -1,4 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_splash/Views/HomeScreen.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -10,10 +13,23 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text("This Is Splash Scrren"),
-      ),
+    return AnimatedSplashScreen(
+      nextScreen: const HomePage(),
+      splash: Lottie.asset('assets/json/loading.json'),
+      // splash: Column(
+      //   children: [
+      //     Image.asset('assets/images/apple.png',width: 150,height: 150,),
+      //     const Text(
+      //       "Apple Application",
+      //       style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+      //     )
+      //   ],
+      // ),
+      backgroundColor: Colors.redAccent,
+      splashIconSize: 250,
+      duration: 3000,
+      animationDuration: const Duration(seconds: 2),
+      splashTransition: SplashTransition.fadeTransition,
     );
   }
 }
