c52395ecfa9654ce053e54907383181b01c89481
Emon Hossain <emondd4@gmail.com>
2022-06-05 10:31:44 +0600

    io client toasted

--- flutter_socket_io/lib/main.dart
+++ flutter_socket_io/lib/main.dart
@@ -1,35 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:socket_io_client/socket_io_client.dart';
 
 
 void main() {
   runApp(const MyApp());
-
-  /// Socket IO
-  // IO.Socket socket = IO.io('https://socketio-chat-h9jt.herokuapp.com/');
-  // socket.onConnect((_) {
-  //   print('connect');
-  //   //socket.emit('msg', 'test');
-  // });
-  // socket.on('event', (data) => print(data));
-  // socket.onDisconnect((_) => print('disconnect'));
-  // socket.on('fromServer', (_) => print(_));
-
-  Socket socket = io('https://socketio-chat-h9jt.herokuapp.com/',
-      OptionBuilder()
-          .setTransports(['websocket']) // for Flutter or Dart VM
-          .disableAutoConnect()  // disable auto-connection
-          .setExtraHeaders({'foo': 'bar'}) // optional
-          .build()
-  );
-  socket.connect();
-  socket.onConnect((data) => {
-    print("Connect"),
-    socket.disconnect(),
-  });
-  socket.onDisconnect((_) => print('disconnect'));
-
 }
 
 class MyApp extends StatelessWidget {
@@ -58,13 +34,66 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  late Socket socket;
+
+  @override
+  void initState() {
+    socket = io('https://socketio-chat-h9jt.herokuapp.com/',
+        OptionBuilder()
+            .setTransports(['websocket']) // for Flutter or Dart VM
+            .disableAutoConnect()  // disable auto-connection
+            .setExtraHeaders({'foo': 'bar'}) // optional
+            .build()
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    socket.onConnect((data) => {
+        _showToast("io client connected")
+    });
+
+    socket.onDisconnect((data) => {
+        _showToast(data)
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {
+              socket.connect();
+            }, child: Text("Connect")),
+            SizedBox(height: 20.0,),
+            ElevatedButton(onPressed: () {
+              socket.disconnect();
+            }, child: Text("Disconnect")),
+            SizedBox(height: 20.0,),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future _showToast(String data) async {
+    Fluttertoast.showToast(
+        msg: data,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+  }
+
 }
